b7263d1365c810270e40916a781bdcc3f00cc319
WMXPY <wm@mengw.io>
2020-04-23 12:10:16 -0500

    initial implement copy

--- src/copy.ts
+++ src/copy.ts
@@ -4,7 +4,32 @@
  * @description Copy
  */
 
-export const copyToClipboard = (): void => {
+import { testClipboardSupport } from "support";
 
-    console.log('test');
+export const copyToClipboardWithClipboardAPI = async (text: string): Promise<boolean> => {
+
+    await navigator.clipboard.writeText(text);
+    return true;
+};
+
+export const copyToClipboardWithExecCommand = (text: string): boolean => {
+
+    const textarea: HTMLTextAreaElement = document.createElement('textarea');
+    textarea.style.display = 'none';
+    textarea.style.height = '0';
+    textarea.style.width = '0';
+
+    textarea.value = text;
+    textarea.select();
+
+    const result: boolean = document.execCommand('copy');
+    return result;
+};
+
+export const copyToClipboard = async (text: string): Promise<boolean> => {
+
+    if (testClipboardSupport()) {
+        return await copyToClipboardWithClipboardAPI(text);
+    }
+    return copyToClipboardWithExecCommand(text);
 };
